d357a8714b9897cd506a05c0c15113ca6cfcf4d2
unknown <josebright29@gmail.com>
2022-04-28 11:45:15 +0000

    updated

--- designs.js
+++ designs.js
@@ -1,40 +1,44 @@
-// Select color input
+//color variable
 const color = $('#colorPicker');
 
-// Select size input
+//Grid size variable
 const size = $('#sizePicker');
 
-// When size is submitted by the user, call makeGrid()
+// Canvas variable
 const canvas = $('#pixelCanvas');
 
+//Color cell function
 function colorCell(event) {
-    const eachCell = $(event.target);
-    const colorSelected = color.val();
-    eachCell.css('background-color', colorSelected);
-  }
-
-function makeGrid(height, width) {
-// Your code goes here!
-  for(let row = 0 ; row < height ; row++) {
-    let rowElement = $('<tr>');
-    for(let col = 0 ; col < width ; col++) {
-      let columnElement = $('<td>');
-      columnElement.on('click', colorCell);
-      rowElement.append(columnElement);
-    }
-    rowElement.appendTo(canvas);
-  }
+	const eachCell = $(event.target);
+	const colorSelected = color.val();
+	eachCell.css('background-color', colorSelected);
 }
 
-function onSubmit(event) {
-    event.preventDefault();
-    const height = $('#inputHeight').val();
-    const width = $('#inputWidth').val();
-    makeGrid(height, width);
-  }
+//m by n grid function
+function makeGrid(height, width) {
+	canvas.empty()
+	for (let row = 0; row < height; row++) {
+		let rowElement = $('<tr>');
+		for (let col = 0; col < width; col++) {
+			let columnElement = $('<td>');
+			columnElement.on('click', colorCell);
+			rowElement.append(columnElement);
+		}
+		rowElement.appendTo(canvas);
+	}
+}
 
+//Submit function
+function onSubmit(event) {
+	event.preventDefault();
+	const height = $('#inputHeight').val();
+	const width = $('#inputWidth').val();
+	makeGrid(height, width);
+}
+
+//Initiate the submit function
 function init() {
-  size.on('submit', onSubmit);
+	size.on('submit', onSubmit);
 }
 
 init();
